c8b0a9ffb7606fa88800a52c24ca494f5b8ed57f
easy2learn <easy2learn.co@gmail.com>
2020-05-22 00:48:38 +0530

    added

--- config/payumoney.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-//   shiva=========================
-  MERCHANT_KEY: "dFVtzjVw",
-  MERCHANT_SALT: "iJnVaHBC1n",
-  AUTHORIZATION_HEADER: "8btlkHLg9Mc8GUG5haCDN2Kqi41hML8WtikJtamok2k=",
-
-}
