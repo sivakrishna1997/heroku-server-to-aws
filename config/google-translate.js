8587e31a71fda060cf1936a86b2a6fb9a16b6d13
easy2learn <easy2learn.co@gmail.com>
2020-05-28 16:09:38 +0530

    added

--- config/google-translate.js
+++
@@ -1,4 +0,0 @@
-// const apiKey = 'AIzaSyDHaP0udQ4x4YupwRzJ8K52LfcNLMPU9aA';
-const apiKey = 'AIzaSyBMU5DFnfYV-iJZXao3cDRYOmG8fKRpR1E'
-
-module.exports = googleTranslate = require('google-translate')(apiKey);
--- config/s3/s3.env.js
+++
@@ -1,6 +0,0 @@
-const env = {
-	AWS_ACCESS_KEY: 'AKIAICCO356V6UKKT3QQ',
-	AWS_SECRET_ACCESS_KEY: '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',
-};
-
-module.exports = env;
